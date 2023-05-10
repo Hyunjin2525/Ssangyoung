da2f650e3af2054de25a4443d7082dc0ec5f0a21
Hyunjin2525 <129152930+Hyunjin2525@users.noreply.github.com>
2023-05-10 18:19:53 +0900

    Delete JavaFinalProject directory

--- JavaFinalProject/src/com/sist/inter/ChatInterface.java
+++
@@ -1,7 +0,0 @@
-package com.sist.inter;
-
-public interface ChatInterface {
-	public void initStyle();
-	public void append(String msg,String color);
-
-}
--- JavaFinalProject/src/com/sist/inter/FindInterface.java
+++
@@ -1,7 +0,0 @@
-package com.sist.inter;
-
-public interface FindInterface {
-	public void findPrint(int cno);
-	public void findPrint2(String title);
-
-}
